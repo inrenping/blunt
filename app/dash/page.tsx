3d0a68baaa01595b229c3465d20e43803fa825b5
inrenping <inrenping@gmail.com>
2025-09-19 20:56:32 +0800

    add demo dash page

--- app/dash/page.tsx
+++ app/dash/page.tsx
@@ -1,9 +1,40 @@
-export default function DashPage() {
-    return (
-        <div className="p-4">
-            <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
-            <p>Welcome to your dashboard! Here you can manage your settings and view your data.</p>
-            {/* Add more dashboard components and features here */}
+import { AppSidebar } from "@/components/dash/app-sidebar"
+import { ChartAreaInteractive } from "@/components/dash/chart-area-interactive"
+import { DataTable } from "@/components/dash/data-table"
+import { SectionCards } from "@/components/dash/section-cards"
+import { SiteHeader } from "@/components/dash/site-header"
+import {
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar"
+
+import data from "./data.json"
+
+export default function dashPage() {
+  return (
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              <SectionCards />
+              <div className="px-4 lg:px-6">
+                <ChartAreaInteractive />
+              </div>
+              <DataTable data={data} />
+            </div>
+          </div>
         </div>
-    );  
+      </SidebarInset>
+    </SidebarProvider>
+  )
 }
