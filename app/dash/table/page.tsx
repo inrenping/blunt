cce80f52386bef8e2a78d27d015d369ca14bfccf
inrenping <inrenping@gmail.com>
2025-09-28 21:03:21 +0800

    update

--- app/dash/table/page.tsx
+++ app/dash/table/page.tsx
@@ -1,5 +1,11 @@
 'use client';
+import { DataTable } from "@/components/dash/data-table"
 
+import data from "../data.json"
 export default function TablePage() {
-  return <div>Table Page</div>
+  return (
+    <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+      <DataTable data={data} />
+    </div>
+  )
 }
